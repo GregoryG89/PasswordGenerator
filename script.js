bf75319e1c3bf1718171c29c36100e0488327667
Gregory Gimalski <asuro89@gmail.com>
2020-10-28 00:38:35 -0400

    added prompts and confirms for questions (working) and layed down ground word for functions

--- script.js
+++ script.js
@@ -7,22 +7,48 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+  
+}
+        var howLong = prompt("Choose the length of the Password!");
+            console.log(howLong);
+        var Lcase = confirm("Do you want lower case in Password?")
+        var Ucase = confirm("Do you want upper case in Password?")
+        var Ncase = confirm("Do you want numbers in Password?")
+        var Scase = confirm("Do you want Special keys in Password?")
+        console.log(Lcase, Ucase, Ncase, Scase)
+        
+         
+        // lowercase -abcdefghijklmnopqrstuvwxyz
+        // uppercase - ABCDEFGHIJKLMNOPQRSTUVWXYZ
+        //number 0123456789
+        // special characters as a string !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+
+        function Lower(){
+        
+        
+        }
+        function Upper(){
+        
+        }
+        function numeric(){
+        
+    }
+        function Special(){
 
 }
-
 var Pass = [];
 var howLong;
 
-var howLong = prompt("Choose the length of the password!");
 
-for(var i =8; i < 127; i++){
 
-    Pass.push(howLong);
-    }
+// for(var i =8; i < 127; i++){
 
-    for(var j = 0; j < Pass.length; j++ ){
-        alert(Pass[j]);
-}
+//     Pass.push(howLong);
+//     }
+
+//     for(var j = 0; j < Pass.length; j++ ){
+//         alert(Pass[j]);
+// }
 
 
 
