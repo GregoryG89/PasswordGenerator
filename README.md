bf7212d65880775c8d4601ecf20397f8fa2a8097
Gregory Gimalski <asuro89@gmail.com>
2020-10-28 22:22:51 -0400

    Added for loop to begging question to repeat until user selects a valid criteria,  also finished the arrays for Lcase Ucase Ncase and Scase

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # PasswordGenerator
 Generates password after prompts
+
+I want to prompt user to input password criteria using prompt command, first one being prompting for a password between 8 - 128 characters
+
+im going to create 4 functions one for each parameter of the password generator ie one of uppercase, one for lower case, one for Num1 and one for special characters.
--- script.js
+++ script.js
@@ -10,60 +10,60 @@ function writePassword() {
 }
  
 
+            // User input and make sure they enter a Num1 8 - 128 or prompts again till they answer
+    var howLong = prompt("Choose the length of the Password! 8 - 128")
+       for( howLong === false; howLong < 8 || howLong > 128; howLong++)   {
+         
 
-    var howLong = parseInt(prompt("Choose the length of the Password!"));
-            console.log(howLong);
-            if (!howLong) {
-                alert("This needs a value")} 
-                else if (howLong < 8 || howLong > 128) {
-                // Validates user input
-                // Start user input prompts
-                howLong = parseInt(prompt("You must choose between 8 and 128"));}
+       }                         
                                                                                 
 
-else {
+
+        var Lcase = confirm("Do you want lower case in Password")
         var Ucase = confirm("Do you want upper case in Password?")
-        var Ncase = confirm("Do you want numbers in Password?")
+        var Ncase = confirm("Do you want Num1s in Password?")
         var Scase = confirm("Do you want Special keys in Password?")
         console.log(Ucase, Ncase, Scase);
+    
+            if((Ucase === false) && Ncase === false && Scase === false && Lcase === false){
+               alert("Must choose a valid criteria!");} 
+
+            //    else if (Ucase === true && Ncase === true && Scase === true){
+            //        var charSet = 
+            //    }
+
+               
+
+
+        
+
+// Special characters array
+special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+// Numeric characters array
+num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// Lowbetical characters array
+lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// Upcase letters array
+upP = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y",]
+
+function generate () {
+    var password = "";
+    for (i = 0; i < 10; i++) {
+        var randomLetter = arr[Math.floor(arr.length * Math.random())];
+        password += randomLetter;
     }
-        
-        var U = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"  ;
-        var N = "0123456789"
-        var S = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
-        
-        // uppercase - ABCDEFGHIJKLMNOPQRSTUVWXYZ
-        //number - 0123456789
-        // special characters as a string !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+    document.getElementById("pw").textContent = password;
+}
 
-        function Upper(){
-        
-        }
-        function numeric(){
-        
-    }
-        function Special(){
-        }
-
-var Pass = [];
-var howLong;
+document.querySelector('#button-clicker').addEventListener('click', function (event) {
+    event.preventDefault();
+    generate();
+});
 
 
 
-// for(var i =8; i < 127; i++){
-
-//     Pass.push(howLong);
-//     }
-
-//     for(var j = 0; j < Pass.length; j++ ){
-//         alert(Pass[j]);
-// }
-
-
-
-
-
-
+for(var q = 0; q < howLong; q++){
+    randomPassword.push(Math.floor(math.ranom) * 10)}
 
 
 
