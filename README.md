fa13e3cee5bf21dfa89f9bc1d62df53d12ff90ce
Gregory Gimalski <asuro89@gmail.com>
2020-10-28 23:49:32 -0400

    Working on finishing touches, button giving a undefined bug, but it is recogonizing the command

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # PasswordGenerator
 Generates password after prompts
 
-I want to prompt user to input password criteria using prompt command, first one being prompting for a password between 8 - 128 special
+I want to prompt user to input password criteria using prompt command, first one being prompting for a password between 8 - 128 special (repeat if they dont answer within the criteria)
 
-im going to create 4 functions one for each parameter of the password generator ie one of uppercase, one for lowererer case, one for numand one for special special
+
+im going to create 4 confirm questions one for each parameter of the password generator ie one of uppercase, one for lowererer case, one for num and one for special character
+Create an Array housing Special chars, uppercase, Lowers case, and numbers
+Create my rules using if else depending on what the user enters using Concat() to pull from multiple strings if neccesary.
+
+Finally i want to create a for loop using length from the prompt of howLong (which was determined by the user) and have it pull from the arrays 
--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// // Assignment Code
+// var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-}
- 
+//   passwordText.value = password;
+// 
 
 // Special character array
 special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
@@ -17,19 +16,12 @@ num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 // lower alpbetical characters array
 lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 // Upcase aphbetical characters array
-upP = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y",]
+upP = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y"];
 
 var placeHold
-    Arr = []
 
-            // User input and make sure they enter a num 8 - 128 or prompts again till they answer
-    var howLong = prompt("Choose the length of the Password! 8 - 128")
-       for( howLong === false; howLong < 8 || howLong > 128; howLong++)   {
-        
-            var howLong = prompt("Choose a password >>BETWEEN<< 8 and 128 please")
-            console.log(howLong)
 
-       }                         
+                               
        var get = document.querySelector("#generate");
 
         get.addEventListener("click", function () {
@@ -40,15 +32,28 @@ var placeHold
                                                                                 
 
 
-        var Lcase = confirm("Do you want lower case in Password")
-        var Ucase = confirm("Do you want upper case in Password?")
-        var Ncase = confirm("Do you want nums in Password?")
-        var Scase = confirm("Do you want Special keys in Password?")
-        console.log(Ucase, Ncase, Scase);
-    
         function generatePassword()
         {
-        if (!Scase && !Ncase && !Ucase && !Lcase) {
+ 
+
+            
+            // User input and make sure they enter a num 8 - 128 or prompts again till they answer
+              var howLong = prompt("Choose the length of the Password! 8 - 128")
+    for( howLong === false; howLong < 8 || howLong > 128; howLong++)   {
+     
+         var howLong = prompt("Choose a password >>BETWEEN<< 8 and 128 please")
+         console.log(howLong);
+
+         Lcase = confirm("Do you want lower case in Password")
+          Ucase = confirm("Do you want upper case in Password?")
+          Ncase = confirm("Do you want nums in Password?")
+          Scase = confirm("Do you want Special keys in Password?")
+         console.log(Ucase, Ncase, Scase);
+     
+
+
+
+            if (!Scase && !Ncase && !Ucase && !Lcase) {
             placeHold = alert("You must choose a criteria!");
     
         }
@@ -113,18 +118,18 @@ var placeHold
         var pickplaceHold = placeHold[Math.floor(Math.random() * placeHold.length)];
         password.push(pickplaceHold);
     }
-        console.log(pickplaceHold)
+    
     // This joins the password array and converts it to a string
     // Worked with a tutor to incorporate this option
     var ps = password.join("");
     UserInput(ps);
     return ps;
 }
+        }
 // This puts the password value into the textbox
 // Changed function input to use textcontent
 function UserInput(ps) {
     document.getElementById("password").textContent = ps;
 
 }
-// Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
+        
