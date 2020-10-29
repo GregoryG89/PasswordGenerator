8e6701199375e46606fc5fc3e15d84d8042f17e8
Gregory Gimalski <asuro89@gmail.com>
2020-10-28 23:24:08 -0400

    Worked on if else statements, got the generaters working but cant get them to output should be done in next push

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # PasswordGenerator
 Generates password after prompts
 
-I want to prompt user to input password criteria using prompt command, first one being prompting for a password between 8 - 128 characters
+I want to prompt user to input password criteria using prompt command, first one being prompting for a password between 8 - 128 special
 
-im going to create 4 functions one for each parameter of the password generator ie one of uppercase, one for lower case, one for Num1 and one for special characters.
+im going to create 4 functions one for each parameter of the password generator ie one of uppercase, one for lowererer case, one for numand one for special special
--- script.js
+++ script.js
@@ -10,62 +10,121 @@ function writePassword() {
 }
  
 
-            // User input and make sure they enter a Num1 8 - 128 or prompts again till they answer
+// Special character array
+special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+// Numeric character array
+num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// lower alpbetical characters array
+lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// Upcase aphbetical characters array
+upP = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y",]
+
+var placeHold
+    Arr = []
+
+            // User input and make sure they enter a num 8 - 128 or prompts again till they answer
     var howLong = prompt("Choose the length of the Password! 8 - 128")
        for( howLong === false; howLong < 8 || howLong > 128; howLong++)   {
-         
+        
+            var howLong = prompt("Choose a password >>BETWEEN<< 8 and 128 please")
+            console.log(howLong)
 
        }                         
+       var get = document.querySelector("#generate");
+
+        get.addEventListener("click", function () {
+     ps = generatePassword();
+      document.getElementById("password").placeholder = ps;
+});
+
                                                                                 
 
 
         var Lcase = confirm("Do you want lower case in Password")
         var Ucase = confirm("Do you want upper case in Password?")
-        var Ncase = confirm("Do you want Num1s in Password?")
+        var Ncase = confirm("Do you want nums in Password?")
         var Scase = confirm("Do you want Special keys in Password?")
         console.log(Ucase, Ncase, Scase);
     
-            if((Ucase === false) && Ncase === false && Scase === false && Lcase === false){
-               alert("Must choose a valid criteria!");} 
+        function generatePassword()
+        {
+        if (!Scase && !Ncase && !Ucase && !Lcase) {
+            placeHold = alert("You must choose a criteria!");
+    
+        }
+        // First if statement that uses user input prompts to determine 
+        // Else if for 4 positive options
+        else if (Scase && Ncase && Ucase && Lcase) {
+    
+            placeHold = special.concat(num, lower, UpP);
+        }
+        // Else if for 3 positive options
+        else if (Scase && Ncase && Ucase) {
+            placeHold = special.concat(num, UpP);
+        }
+        else if (Scase && Ncase && Lcase) {
+            placeHold = special.concat(num, lower);
+        }
+        else if (Scase && Lcase && Ucase) {
+            placeHold = special.concat(lower, UpP);
+        }
+        else if (Ncase && Lcase && Ucase) {
+            placeHold = num.concat(lower, UpP);
+        }
+        // Else if for 2 positive options 
+        else if (Scase && Ncase) {
+            placeHold = special.concat(num);
+    
+        } else if (Scase && Lcase) {
+            placeHold = special.concat(lower);
+    
+        } else if (Scase && Ucase) {
+            placeHold = special.concat(UpP);
+        }
+        else if (Lcase && Ncase) {
+            placeHold = lower.concat(num);
+    
+        } else if (Lcase && Ucase) {
+            placeHold = lower.concat(UpP);
+    
+        } else if (Ncase && Ucase) {
+            placeHold = num.concat(UpP);
+        }
+   
+        else if (Scase) {
+            placeHold = special;
+        }
+        else if (Ncase) {
+            placeHold = num;
+        }
+        else if (Lcase) {
+            placeHold = lower;
+        }
+        // Created space variable to fill uppercase conversion
+        else if (Ucase) {
+            placeHold = Upp;
+        };
+    
+        var password = [];
 
-            //    else if (Ucase === true && Ncase === true && Scase === true){
-            //        var charSet = 
-            //    }
-
-               
-
-
-        
-
-// Special characters array
-special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
-// Numeric characters array
-num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-// Lowbetical characters array
-lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-// Upcase letters array
-upP = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y",]
-
-function generate () {
-    var password = "";
-    for (i = 0; i < 10; i++) {
-        var randomLetter = arr[Math.floor(arr.length * Math.random())];
-        password += randomLetter;
+    // Start random selection variables:
+    // Random selection for all variables: 
+    for (var i = 0; i < howLong; i++) {
+        var pickplaceHold = placeHold[Math.floor(Math.random() * placeHold.length)];
+        password.push(pickplaceHold);
     }
-    document.getElementById("pw").textContent = password;
+        console.log(pickplaceHold)
+    // This joins the password array and converts it to a string
+    // Worked with a tutor to incorporate this option
+    var ps = password.join("");
+    UserInput(ps);
+    return ps;
 }
+// This puts the password value into the textbox
+// Changed function input to use textcontent
+function UserInput(ps) {
+    document.getElementById("password").textContent = ps;
 
-document.querySelector('#button-clicker').addEventListener('click', function (event) {
-    event.preventDefault();
-    generate();
-});
-
-
-
-for(var q = 0; q < howLong; q++){
-    randomPassword.push(Math.floor(math.ranom) * 10)}
-
-
-
+}
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// generateBtn.addEventListener("click", writePassword);
